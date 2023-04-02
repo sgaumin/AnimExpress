51388b38fd91899374bf3d8a5c60b6620e8194d1
Sébastien Gaumin <sgaumin@gmail.com>
2023-04-02 23:58:57 +0900

    Remove unused drag and drop area

--- Editor/AnimationExpressCustomEditor.cs
+++ Editor/AnimationExpressCustomEditor.cs
@@ -41,36 +41,6 @@ namespace AnimExpressEditor
 			EditorGUILayout.PropertyField(serializedObject.FindProperty("frames"));
 			EditorGUILayout.PropertyField(serializedObject.FindProperty("events"));
 
-			GUIStyle GuistyleBoxDND = new GUIStyle(GUI.skin.box);
-			GuistyleBoxDND.alignment = TextAnchor.MiddleCenter;
-			GuistyleBoxDND.fontStyle = FontStyle.Italic;
-			GuistyleBoxDND.fontSize = 12;
-			GUI.skin.box = GuistyleBoxDND;
-
-			EditorGUILayout.Space(32f);
-
-			Rect myRect = GUILayoutUtility.GetRect(0, 140, GUILayout.ExpandWidth(true));
-			GUI.Box(myRect, "Drag and Drop Sprites to this Box!", GuistyleBoxDND);
-			if (myRect.Contains(Event.current.mousePosition))
-			{
-				if (Event.current.type == EventType.DragUpdated)
-				{
-					DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-					Event.current.Use();
-				}
-				else if (Event.current.type == EventType.DragPerform)
-				{
-					for (int i = 0; i < DragAndDrop.objectReferences.Length; i++)
-					{
-						if (DragAndDrop.objectReferences[i] is Sprite sprite)
-						{
-							context.AddFrame(sprite);
-						}
-					}
-					Event.current.Use();
-				}
-			}
-
 			serializedObject.ApplyModifiedProperties();
 		}
 	}
